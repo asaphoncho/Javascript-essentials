ea10728668f96b7db4e55af7b2d0f531c973202c
asaphoncho <uniqueeph38@gmail.com>
2024-06-09 06:58:36 -0400

    Restaurant Menu

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -13,4 +13,7 @@ function checkCountValue() {
     } else if (count === 20) {
       alert("Your Instagram post gained 20 followers! Keep it up!");
     }
-  }
+}
+function resetCount(){
+    count = 0
+}
